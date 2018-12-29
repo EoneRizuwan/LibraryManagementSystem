3e074f0116229a8a308c4a9b5cf522a4572a8281
EoneRizuwan <mhmmdrzwn31@gmail.com>
2018-12-30 00:07:08 +0800

    Ep-1 Developing UI for adding books

---
+++ README.md
@@ -0,0 +1,4 @@
+#Library Management System
+
+A JavaFX tutorial for library management system following
+Genuine Coder's playlist.
---
+++ src/main/java/com/anubiscode/libraryx/modelview/addbook/AddbookController.java
@@ -0,0 +1,27 @@
+package main.java.com.anubiscode.libraryx.modelview.addbook;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AddbookController {
+
+    @FXML
+    private JFXTextField title;
+    @FXML
+    private JFXTextField id;
+    @FXML
+    private JFXTextField author;
+    @FXML
+    private JFXTextField publisher;
+
+    @FXML
+    private void clearField(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void saveBook(ActionEvent event) {
+
+    }
+}
